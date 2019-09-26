dee116e4567a0b21df6827c0f2204a1ce0e3ae1b
Chris Nielsen <chris.relaxing@gmail.com>
2019-09-25 21:07:33 -0500

    Added greeting and number input.

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,7 +6,20 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nunber Guesser ");
+            ColorMessage(ConsoleColor.Cyan, "Welcome to the \"Number Guesser\" interactive console experience: ");
+
+            // Get user's name and greet them
+            GreetUser();
+
+            Console.WriteLine("\nEnter a number between 1 and 10:");
+
+            // Get the number
+            string inputNum = Console.ReadLine();
+
+            int myGuess = 0;
+            myGuess = Int32.Parse(inputNum);
+
+            Console.WriteLine("\nYou chose the number {0}", myGuess);
         }
 
         static void ColorMessage(ConsoleColor color, string message)
@@ -18,7 +31,19 @@ namespace NumberGuesser
             Console.WriteLine(message);
 
             // Reset text color
+            Console.ResetColor();
+        }
 
+        static void GreetUser()
+        {
+            // Ask for user's name
+            Console.WriteLine("\nWhat is your name?");
+
+            // Get the user's name
+            string userName = Console.ReadLine();
+
+            // Use substitution in a string for the greeting
+            Console.WriteLine("\nHello {0}, are you ready to play? :)", userName);
         }
     }
 }
