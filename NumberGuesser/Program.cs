2e70e358c9cc2137fb2d8c74c24eadd451bd23ef
Chris Nielsen <chris.relaxing@gmail.com>
2019-09-25 20:12:00 -0500

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nunber Guesser ");
+        }
+
+        static void ColorMessage(ConsoleColor color, string message)
+        {
+            // Set text color
+            Console.ForegroundColor = color;
+
+            // Set message
+            Console.WriteLine(message);
+
+            // Reset text color
+
+        }
+    }
+}
