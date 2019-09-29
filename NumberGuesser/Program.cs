646c022d64c916f3e141141a2b2e22f27a50ec0d
Chris Nielsen <chris.relaxing@gmail.com>
2019-09-29 12:16:04 -0500

    All basic functionality completed.

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -11,26 +11,31 @@ namespace NumberGuesser
             // Get user's name and greet them
             GreetUser();
 
-            //To Do:
-            //Make random go from 1 to 100
-            //Logic to say whether to go higher or lower
-            //Keep a count of the number of guesses
-            //Keep track of each users best score (lowest number of guesses)
+            // Features:
+            // Random number between 1 and 100
+            // Logic to say whether to go higher or lower
+            // Keep a count of the number of guesses
 
+            // To Do:
+            // Keep track of each users best score (lowest number of guesses)
 
             while (true)
             {
                 // Create a new Random object
                 Random random = new Random();
 
-                // Let random choose the correct number
-                int correctNumber = random.Next(1, 10);
+                int numberRange = 100;
 
-                // Console.WriteLine("\nEnter a number between 1 and 10: ({0})", correctNumber);
-                Console.WriteLine("\nEnter a number between 1 and 10: ");
+                // Let random choose the correct number
+                int correctNumber = random.Next(1, numberRange);
+
+                //Console.WriteLine("\nEnter a number between 1 and {0}: ({1})", numberRange, correctNumber);
+                Console.WriteLine("\nEnter a number between 1 and {0}:", numberRange);
+                //Console.WriteLine("\nEnter a number between 1 and 10: ");
 
                 // Initialize the variable myGuess
                 int myGuess = 0;
+                int tryCount = 1;
 
                 while (myGuess != correctNumber)
                 {
@@ -54,13 +59,22 @@ namespace NumberGuesser
                     {
                         // Reflect the number that the user chose
                         //Console.WriteLine("\n{0} is not correct. Try again.", myGuess);
+                        if (myGuess < correctNumber)
+                        {
+                            string textMessage = "\n" + myGuess + " is not correct. Try a higher number.";
+                            ColorMessage(ConsoleColor.Red, textMessage);
+                        }
+                        else
+                        {
+                            string textMessage = "\n" + myGuess + " is not correct. Try a lower number.";
+                            ColorMessage(ConsoleColor.Red, textMessage);
+                        }
 
-                        string textMessage = "\n" + myGuess + " is not correct. Try again.";
-                        ColorMessage(ConsoleColor.Red, textMessage);
+                        tryCount += 1;
                     } 
                     else
                     {
-                        string textMessage = "\nCorrect! Good job.";
+                        string textMessage = "\nGood job! You found the correct answer (" + correctNumber + ") in " + tryCount + " tries.";
                         ColorMessage(ConsoleColor.Green, textMessage);
                     }
                     
@@ -94,7 +108,7 @@ namespace NumberGuesser
             string userName = Console.ReadLine();
 
             // Use substitution in a string for the greeting
-            Console.WriteLine("\nHello {0}, are you ready to play? :)", userName);
+            Console.WriteLine("\nHello {0}, thanks for playing!", userName);
         }
     }
 }
